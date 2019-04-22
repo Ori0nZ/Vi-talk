9088d738fab2ebfc2ffb2fc9ae91b2a849293674
Ori0nZ <manhtranduc1111@gmail.com>
2019-04-22 10:49:26 +0300

    add file

---
+++ config.ts
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyDdUlOI35BXR8Y4qior3HdSzqezGcn3Kls",
+  authDomain: "chatapp-7fca9.firebaseapp.com",
+  databaseURL: "https://chatapp-7fca9.firebaseio.com",
+  projectId: "chatapp-7fca9",
+  storageBucket: "chatapp-7fca9.appspot.com",
+  messagingSenderId: "849505520042",
+};
+
+export default config;
